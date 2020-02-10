4c320caf800c4b3a133d37062440b07b20ccc504
Lital Levin <lital.levin@gmail.com>
2020-02-10 16:22:18 +0100

    des chiffres et des lettres

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,17 +7,42 @@ class GamesController < ApplicationController
 
   def new
     @letters = (0...8).map { (65 + rand(26)).chr }
-
   end
 
   def score
-    @word = params["word"]
-
-    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
-
-    word_serialized = open(url).read
-    @word = JSON.parse(word_serialized)
+    @word = params[:word]
 
 
+    @letters = params[:grid]
+
+
+    if is_included_in_grid?(@word, @letters) == false
+      @result = "Letters are not included in the grid"
+    else
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      word_serialized = open(url).read
+      @word = JSON.parse(word_serialized)
+
+      if @word["found"] == false
+        @result = "Word not found"
+      else
+        @result = "Your word is #{@word["word"]}/n
+                    Time taken to answer: /n
+        "
+      end
+    end
   end
+
+  private
+
+  def is_included_in_grid?(word, letters)
+    word = word.upcase
+    word.chars.all? do |char|
+      word.chars.count(char) <= letters.count(char)
+    end
+  end
+
+
+
+
 end
